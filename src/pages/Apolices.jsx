00fc5aa0000832405e537708e9cb6b5e12a60a7b
vitorpradof <vitorprado.ferreira@hotmail.com>
2021-07-03 00:44:53 -0300

    Adição de apólice

--- src/pages/Apolices.jsx
+++ src/pages/Apolices.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
 
-import { Row, Col, Table, Modal, ModalHeader, ModalBody, ModalFooter, Button, Form, Label, FormGroup, Input } from 'reactstrap';
+import { Row, Col, Table, Modal, ModalHeader, ModalBody, ModalFooter, Button, Form, Label, FormGroup, Input, Badge } from 'reactstrap';
 import { FaPen, FaPlus, FaExclamationCircle } from 'react-icons/fa';
 import { ImSpinner8 } from 'react-icons/im';
 import { toast } from 'react-toastify';
+import moment from 'moment';
 
 function Apolices() {
 
   const [iniciando, setIniciando] = useState(true);
   const [apolices, setApolices] = useState([]);
+  const [novaApolice, setNovaApolice] = useState({ apoliceNumero: 0, placa: "", vigenciaInicio: "", vigenciaFim: "", valor: "" });
+  const [exibirModalCriarApolice, setExibirModalCriarApolice] = useState(false);
 
   useEffect (() => {
     getApolices();
@@ -17,6 +20,17 @@ function Apolices() {
   useEffect (() => {
     if (apolices.length) localStorage.setItem("apolices", JSON.stringify(apolices));
   }, [apolices]);
+
+  function criarId() {
+    let stringAleatoria = '',
+        caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < 24; i++) {
+      stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    }
+
+    return stringAleatoria;
+  }
 
   function getApolices() {
     const auxApolices = localStorage.getItem("apolices");
@@ -32,6 +46,36 @@ function Apolices() {
     return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   }
 
+  function calcularDiasDoVencimento() {
+    let diferenca = moment(novaApolice.vigenciaFim, "YYYY/MM/DD").diff(moment(novaApolice.vigenciaInicio, "YYYY/MM/DD"));
+    return moment.duration(diferenca).asDays();
+  }
+
+  function renderizarStatus() {
+    return calcularDiasDoVencimento() > 0 ? "ABERTA" : "VENCIDA";
+  }
+
+  function handleClickFecharModalCadastrarApolice() {
+    setExibirModalCriarApolice(false);
+    setNovaApolice({ apoliceNumero: 0, placa: "", vigenciaInicio: "", vigenciaFim: "", valor: "" });
+  }
+
+  function handleSubmitCadastrarApolice(e) {
+    e.preventDefault();
+    
+    let auxApolice = { 
+      id: criarId(),
+      ...novaApolice,
+      diasAteOVencimento: calcularDiasDoVencimento(),
+      status: renderizarStatus()
+    };
+
+    setApolices([ ...apolices, auxApolice ]);
+    setNovaApolice({ apoliceNumero: 0, placa: "", vigenciaInicio: "", vigenciaFim: "", valor: "" });
+    setExibirModalCriarApolice(false);
+    toast.success("Apólice cadastrada com sucesso!");
+  }
+
   return (
     <Row>
       <Col sm={12}>
@@ -41,7 +85,7 @@ function Apolices() {
               <div className="d-flex justify-content-center align-items-center"><ImSpinner8 size={12} className="fa-spin" style={{ marginRight: 5 }}/> Carregando</div>
             : 
             <div>
-              <Button type="button" color="success" style={{ marginBottom: 15 }}> <FaPlus size={12} /> Nova apolice</Button>
+              <Button type="button" color="success" style={{ marginBottom: 15 }} onClick={() => setExibirModalCriarApolice(true)}> <FaPlus size={12} /> Nova apolice</Button>
               { 
                 apolices.length ?
                   <Table hover responsive striped size="sm">
@@ -59,11 +103,11 @@ function Apolices() {
                       {
                         apolices.map(apolice => 
                           <tr key={apolice.id}>
-                            <td style={{ verticalAlign: 'middle' }}>{apolice.numero}</td>
+                            <td style={{ verticalAlign: 'middle' }}>{apolice.apoliceNumero}</td>
                             <td style={{ verticalAlign: 'middle' }}>{apolice.placa}</td>
                             <td style={{ verticalAlign: 'middle' }}>{renderizarData(apolice.vigenciaInicio)} até {renderizarData(apolice.vigenciaFim)}</td>
-                            <td style={{ verticalAlign: 'middle' }}>{apolice.diasAteOVencimento}</td>
-                            <td style={{ verticalAlign: 'middle' }}>{renderizarValor(apolice.valor)}</td>
+                            <td style={{ verticalAlign: 'middle' }}>{apolice.diasAteOVencimento} <Badge color={apolice.status === "VENCIDA" ? "danger" : "success"}>{apolice.status}</Badge></td>
+                            <td style={{ verticalAlign: 'middle' }}>R${renderizarValor(apolice.valor)}</td>
                             <td style={{ verticalAlign: 'middle' }}>
                               <Button color="link"><FaPen size={14} /> Editar</Button>
                             </td>
@@ -78,6 +122,53 @@ function Apolices() {
           }
         </div>
       </Col>
+      <Modal isOpen={exibirModalCriarApolice}>
+        <ModalHeader toggle={() => handleClickFecharModalCadastrarApolice()}>Nova apólice</ModalHeader>
+        <Form onSubmit={handleSubmitCadastrarApolice}>
+          <ModalBody>
+            <Row>
+              <Col sm="12" md="4">
+                <FormGroup>
+                  <Label for="input-apoliceNumero"><span className="span-obrigatorio">*</span> Número</Label>
+                  <Input type="number" id="input-apoliceNumero" value={novaApolice.apoliceNumero} onChange={e => setNovaApolice({ ...novaApolice, apoliceNumero: e.target.value })} required />
+                </FormGroup>
+              </Col>
+              <Col sm="12" md="8">
+                <FormGroup>
+                  <Label for="input-placa"><span className="span-obrigatorio">*</span> Placa</Label>
+                  <Input type="text" id="input-placa" value={novaApolice.placa} onChange={e => setNovaApolice({ ...novaApolice, placa: e.target.value })} required />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm="12" md="6">
+                <FormGroup>
+                  <Label for="input-vigenciaInicio"><span className="span-obrigatorio">*</span> Início da vigência</Label>
+                  <Input type="date" id="input-vigenciaInicio" value={novaApolice.vigenciaInicio} onChange={e => setNovaApolice({ ...novaApolice, vigenciaInicio: e.target.value })} required />
+                </FormGroup>
+              </Col>
+              <Col sm="12" md="6">
+                <FormGroup>
+                  <Label for="input-vigenciaFim"><span className="span-obrigatorio">*</span> Fim da vigência</Label>
+                  <Input type="date" id="input-vigenciaFim" value={novaApolice.vigenciaFim} onChange={e => setNovaApolice({ ...novaApolice, vigenciaFim: e.target.value })} required />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm="12" md="6">
+                <FormGroup>
+                  <Label for="input-valor"><span className="span-obrigatorio">*</span> Valor</Label>
+                  <Input type="text" id="input-valor" value={novaApolice.valor} onChange={e => setNovaApolice({ ...novaApolice, valor: e.target.value })} required />
+                </FormGroup>
+              </Col>
+            </Row>
+          </ModalBody>
+          <ModalFooter>
+            <Button type="button" outline color="secondary" onClick={() => handleClickFecharModalCadastrarApolice()}>Cancelar</Button>{' '}
+            <Button type="submit" color="success">Cadastrar</Button>
+          </ModalFooter>
+        </Form>
+      </Modal>
     </Row>
   );
 }
