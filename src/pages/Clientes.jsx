405322282dd5caf5286ac8bd23fbd721e90caa39
vitorpradof <vitorprado.ferreira@hotmail.com>
2021-07-02 23:04:15 -0300

    Editar cliente

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -8,16 +8,22 @@ function Clientes() {
 
   const [iniciando, setIniciando] = useState(true);
   const [clientes, setClientes] = useState([]);
-  const [novoCliente, setNovoCliente] = useState({ nome: "", sobrenome: "", cpf: "", cidade: "", uf: "" });
+  const [clienteSelecionado, setClienteSelecionado] = useState({});
   const [exibirModalCriarCliente, setExibirModalCriarCliente] = useState(false);
+  const [exibirModalEditarCliente, setExibirModalEditarCliente] = useState(false);
 
   useEffect (() => {
+    resetarClienteSelecionado();
     getClientes();
   }, []);
 
   useEffect (() => {
     if (clientes.length) localStorage.setItem("clientes", JSON.stringify(clientes));
   }, [clientes]);
+
+  function resetarClienteSelecionado() {
+    setClienteSelecionado({ nome: "", sobrenomes: "", cpf: "", cidade: "", uf: "" });
+  }
 
   function criarId() {
     let stringAleatoria = '',
@@ -36,24 +42,76 @@ function Clientes() {
     setIniciando(false);
   }
 
+  function handleClickFecharModalCadastrarCliente() {
+    setExibirModalCriarCliente(false)
+    resetarClienteSelecionado();
+  }
+
+  function handleClickFecharModalEditarCliente() {
+    setExibirModalEditarCliente(false)
+    resetarClienteSelecionado();
+  }
+
   function handleSubmitCadastrarCliente(e) {
     e.preventDefault();
     
-    let arraySobrenomes = novoCliente.sobrenome.split(" "),
-        ultimoNome = arraySobrenomes[arraySobrenomes.lenght - 1],
+    let arraySobrenomes = clienteSelecionado.sobrenomes.split(" "),
+        ultimoNome = arraySobrenomes[arraySobrenomes.length - 1],
         auxCliente = { 
           id: criarId(),
-          ...novoCliente,
+          ...clienteSelecionado,
           nome: {
-            primeiroNome: novoCliente.nome,
+            primeiroNome: clienteSelecionado.nome,
             sobrenomes: arraySobrenomes,
             ultimoNome
           } 
         };
 
-    delete auxCliente.sobrenome;
+    delete auxCliente.sobrenomes;
 
     setClientes([ ...clientes, auxCliente ]);
+    resetarClienteSelecionado();
+    setExibirModalCriarCliente(false);
+  }
+
+  function handleClickEditarCliente (cliente) {
+    let auxCliente = { ...cliente };
+
+    auxCliente.nome = cliente.nome.primeiroNome;
+    auxCliente.sobrenomes = "";
+
+    cliente.nome.sobrenomes.map((sobrenome, i) => {
+      if (i > 0) auxCliente.sobrenomes += " ";
+      auxCliente.sobrenomes += sobrenome;
+    });
+
+
+    setClienteSelecionado(auxCliente);
+    setExibirModalEditarCliente(true);
+  }
+
+  function handleSubmitEditarCliente(e) {
+    e.preventDefault();
+    
+    let arraySobrenomes = clienteSelecionado.sobrenomes.split(" "),
+        ultimoNome = arraySobrenomes[arraySobrenomes.length - 1],
+        auxCliente = { 
+          ...clienteSelecionado,
+          nome: {
+            primeiroNome: clienteSelecionado.nome,
+            sobrenomes: arraySobrenomes,
+            ultimoNome
+          } 
+        },
+        auxClientes = [ ...clientes ];
+    
+    auxClientes = auxClientes.filter(c => { return c.id !== auxCliente.id });
+
+    delete auxCliente.sobrenomes;
+
+    setClientes([ ...auxClientes, auxCliente ]);
+    resetarClienteSelecionado();
+    setExibirModalCriarCliente(false);
   }
 
   return (
@@ -85,7 +143,7 @@ function Clientes() {
                             <td style={{ verticalAlign: 'middle' }}>{cliente.cpf}</td>
                             <td style={{ verticalAlign: 'middle' }}>{cliente.cidade}/{cliente.uf}</td>
                             <td style={{ verticalAlign: 'middle' }}>
-                              <Button color="link"><FaPen size={14}/> Editar</Button>
+                              <Button color="link" onClick={() => handleClickEditarCliente(cliente)}><FaPen size={14} /> Editar</Button>
                             </td>
                           </tr>
                         )
@@ -99,20 +157,20 @@ function Clientes() {
         </div>
       </Col>
       <Modal isOpen={exibirModalCriarCliente}>
-        <ModalHeader toggle={() => setExibirModalCriarCliente(false)}>Novo cliente</ModalHeader>
+        <ModalHeader toggle={() => handleClickFecharModalCadastrarCliente()}>Novo cliente</ModalHeader>
         <Form onSubmit={handleSubmitCadastrarCliente}>
           <ModalBody>
             <Row>
               <Col sm="12" md="6">
                 <FormGroup>
                   <Label for="input-nome"><span className="span-obrigatorio">*</span> Nome</Label>
-                  <Input type="text" id="input-nome" value={novoCliente.nome} onChange={e => setNovoCliente({ ...novoCliente, nome: e.target.value })} required />
+                  <Input type="text" id="input-nome" value={clienteSelecionado.nome} onChange={e => setClienteSelecionado({ ...clienteSelecionado, nome: e.target.value })} required />
                 </FormGroup>
               </Col>
               <Col sm="12" md="6">
                 <FormGroup>
-                  <Label for="input-sobrenome"><span className="span-obrigatorio">*</span> Sobrenome</Label>
-                  <Input type="text" id="input-sobrenome" value={novoCliente.sobrenome} onChange={e => setNovoCliente({ ...novoCliente, sobrenome: e.target.value })} required />
+                  <Label for="input-sobrenomes"><span className="span-obrigatorio">*</span> Sobrenome</Label>
+                  <Input type="text" id="input-sobrenomes" value={clienteSelecionado.sobrenomes} onChange={e => setClienteSelecionado({ ...clienteSelecionado, sobrenomes: e.target.value })} required />
                 </FormGroup>
               </Col>
             </Row>
@@ -120,13 +178,13 @@ function Clientes() {
               <Col sm="12" md="8">
                 <FormGroup>
                   <Label for="input-cidade"><span className="span-obrigatorio">*</span> Cidade</Label>
-                  <Input type="text" id="input-cidade" value={novoCliente.cidade} onChange={e => setNovoCliente({ ...novoCliente, cidade: e.target.value })} required />
+                  <Input type="text" id="input-cidade" value={clienteSelecionado.cidade} onChange={e => setClienteSelecionado({ ...clienteSelecionado, cidade: e.target.value })} required />
                 </FormGroup>
               </Col>
               <Col sm="12" md="4">
                 <FormGroup>
                   <Label for="input-uf"><span className="span-obrigatorio">*</span> Estado</Label>
-                  <Input type="text" id="input-uf" value={novoCliente.uf} onChange={e => setNovoCliente({ ...novoCliente, uf: e.target.value })} required />
+                  <Input type="text" id="input-uf" value={clienteSelecionado.uf} onChange={e => setClienteSelecionado({ ...clienteSelecionado, uf: e.target.value })} required />
                 </FormGroup>
               </Col>
             </Row>
@@ -134,14 +192,61 @@ function Clientes() {
               <Col sm="12">
                 <FormGroup>
                   <Label for="input-cpf"><span className="span-obrigatorio">*</span> CPF</Label>
-                  <Input type="text" id="input-cpf" value={novoCliente.cpf} onChange={e => setNovoCliente({ ...novoCliente, cpf: e.target.value })} required />
+                  <Input type="text" id="input-cpf" value={clienteSelecionado.cpf} onChange={e => setClienteSelecionado({ ...clienteSelecionado, cpf: e.target.value })} required />
                 </FormGroup>
               </Col>
             </Row>
           </ModalBody>
           <ModalFooter>
-            <Button type="button" outline color="secondary" onClick={() => setExibirModalCriarCliente(false)}>Cancelar</Button>{' '}
+            <Button type="button" outline color="secondary" onClick={() => handleClickFecharModalCadastrarCliente()}>Cancelar</Button>{' '}
             <Button type="submit" color="success">Cadastrar</Button>
+          </ModalFooter>
+        </Form>
+      </Modal>
+      <Modal isOpen={exibirModalEditarCliente}>
+        <ModalHeader toggle={() => handleClickFecharModalEditarCliente()}>Editar cliente</ModalHeader>
+        <Form onSubmit={handleSubmitEditarCliente}>
+          <ModalBody>
+            <Row>
+              <Col sm="12" md="6">
+                <FormGroup>
+                  <Label for="input-nome"><span className="span-obrigatorio">*</span> Nome</Label>
+                  <Input type="text" id="input-nome" value={clienteSelecionado.nome} onChange={e => setClienteSelecionado({ ...clienteSelecionado, nome: e.target.value })} required />
+                </FormGroup>
+              </Col>
+              <Col sm="12" md="6">
+                <FormGroup>
+                  <Label for="input-sobrenomes"><span className="span-obrigatorio">*</span> Sobrenome</Label>
+                  <Input type="text" id="input-sobrenomes" value={clienteSelecionado.sobrenomes} onChange={e => setClienteSelecionado({ ...clienteSelecionado, sobrenomes: e.target.value })} required />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm="12" md="8">
+                <FormGroup>
+                  <Label for="input-cidade"><span className="span-obrigatorio">*</span> Cidade</Label>
+                  <Input type="text" id="input-cidade" value={clienteSelecionado.cidade} onChange={e => setClienteSelecionado({ ...clienteSelecionado, cidade: e.target.value })} required />
+                </FormGroup>
+              </Col>
+              <Col sm="12" md="4">
+                <FormGroup>
+                  <Label for="input-uf"><span className="span-obrigatorio">*</span> Estado</Label>
+                  <Input type="text" id="input-uf" value={clienteSelecionado.uf} onChange={e => setClienteSelecionado({ ...clienteSelecionado, uf: e.target.value })} required />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm="12">
+                <FormGroup>
+                  <Label for="input-cpf"><span className="span-obrigatorio">*</span> CPF</Label>
+                  <Input type="text" id="input-cpf" value={clienteSelecionado.cpf} onChange={e => setClienteSelecionado({ ...clienteSelecionado, cpf: e.target.value })} required />
+                </FormGroup>
+              </Col>
+            </Row>
+          </ModalBody>
+          <ModalFooter>
+            <Button type="button" outline color="secondary" onClick={() => handleClickFecharModalEditarCliente()}>Cancelar</Button>{' '}
+            <Button type="submit" color="success">Editar</Button>
           </ModalFooter>
         </Form>
       </Modal>
